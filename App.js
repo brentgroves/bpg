fd1404ae32008d56877f795d86853b69586a03ac
Brent Groves <brent.groves@gmail.com>
2017-12-11 15:04:50 -0500

    12-11-2017 Multiple templates

---
+++ App.js
@@ -0,0 +1,396 @@
+import React, { Component } from 'react'
+import ReactDOM from 'react-dom'
+import { Link, withRouter } from 'react-router-dom'
+import Routes from './Routes'
+import { authUser, signOutUser } from './libs/awsLib'
+import './App.css'
+import MyAccord from './containers/MyAccord'
+// import { Grid} from "react-bootstrap";
+import { Message, Dropdown, Sidebar, Segment, Button, Image, Header, Accordion, Icon, List, Menu } from 'semantic-ui-react'
+
+let jsreport = require('jsreport-browser-client-dist')
+jsreport.serverUrl = 'http://localhost:5488'
+
+// import '../sass/main.scss';
+// https://www.npmjs.com/package/react-split-pane
+
+
+// var ReactGridLayout = require('react-grid-layout');
+
+
+class App extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      isAuthenticated: false,
+      isAuthenticating: true,
+      activeItem: 0,
+      rptStep: 2,
+      sidebarvisible: false,
+      sidebar: 'production'
+    }
+
+
+    // This binding is necessary to make `this` work in the callback
+    this.handleLogout = this.handleLogout.bind(this)
+    // This binding is necessary to make `this` work in the callback
+    this.setSidebarVisible = this.setSidebarVisible.bind(this)
+    // This binding is necessary to make `this` work in the callback
+    this.setRptStep = this.setRptStep.bind(this)
+    // This binding is necessary to make `this` work in the callback
+    this.getRptStep = this.getRptStep.bind(this)
+    // This binding is necessary to make `this` work in the callback
+    this.rmReport = this.rmReport.bind(this)
+    // window.onresize = this.handleResize;
+  }
+  async componentDidMount() {
+    try {
+      if (await authUser()) {
+        this.userHasAuthenticated(true);
+        let request = {
+            template: {
+              name: 'HtmlToBrowserClient'
+            },
+            data: {
+              rptName: 'DashBoard'
+            }
+          }
+
+        // add custom headers to ajax calls
+        jsreport.headers.Authorization = 'Basic ' + btoa('admin:password')
+        jsreport.render('detail', request)
+      }
+    } catch (e) {
+      alert(e)
+    }
+
+    this.setState({ isAuthenticating: false })
+  }
+
+  userHasAuthenticated = authenticated => {
+    this.setState({ isAuthenticated: authenticated })
+  }
+
+  rmReport = () => {
+    let detail = document.getElementById('detail')
+    detail.innerHTML = ''
+  }
+
+  setRptStep = rptStep => {
+  	if (rptStep === 1) {
+	    let detail = document.getElementById('detail')
+	    detail.innerHTML = ''
+  	}
+    this.setState({ rptStep: rptStep })
+  }
+  getRptStep = () => {
+    return this.state.rptStep
+  }
+
+  setSidebarVisible = visible => {
+    this.setState({ sidebarVisible: visible })
+  }
+
+  getRptStep = () => {
+    return this.state.rptStep
+  }
+
+
+  handleLogout = (event) => {
+    this.rmReport()
+    signOutUser()
+    this.userHasAuthenticated(false)
+    this.setState({ sidebarVisible: false })
+    this.props.history.push('/login')
+  }
+
+
+  render() {
+    const childProps = {
+      isAuthenticated: this.state.isAuthenticated,
+      userHasAuthenticated: this.userHasAuthenticated,
+      handleLogout: this.handleLogout,
+      setRptStep: this.setRptStep,
+      getRptStep: this.getRptStep,
+      rmReport: this.rmReport,
+      sidebarVisible: this.sidebarVisible,
+      setSidebarVisible: this.setSidebarVisible
+
+    }
+
+    const { activeItem, sidebarVisible } = this.state
+    // const visible = true;
+    let divStyle = {
+      width: '100%',
+      height: '100%',
+      minHeight: '100%'
+      //      width: '100%'
+    }
+
+
+
+    return (
+      <div style={divStyle} className='mycontainer'>
+
+
+        <Menu fluid attached='top'>
+          {this.state.isAuthenticated ?
+            <Dropdown item name='sidebar' icon='sidebar'
+            closeOnChange={true}
+            simple
+              onClick={(e, v) => {
+                this.setState({ sidebarVisible: !this.state.sidebarVisible })
+              }} >
+              <Dropdown.Menu>
+
+                <Dropdown.Item
+                  onFocus={(e, dropdown)=>{
+                  }}
+                  onClick={(e, dropdown) => {
+                    e.stopPropagation()
+                    this.setState({ sidebar: 'production' })
+                    this.setState({ sidebarVisible: true })
+                  }}
+
+
+                >
+                  <span className='text'>Production</span>
+                </Dropdown.Item>
+                <Dropdown.Item
+                  onFocus={(e, dropdown)=>{
+                  }}
+                  onClick={(e, dropdown) => {
+                    e.stopPropagation()
+                    this.setState({ sidebar: 'purchasing' })
+                    this.setState({ sidebarVisible: true })
+                  }}
+
+                >
+                  <span className='text'>Purchasing</span>
+                </Dropdown.Item>
+
+              </Dropdown.Menu>
+            </Dropdown>
+            : 
+            [
+              <Menu.Item
+                name='signup'
+                onClick={() => {
+                  childProps.rmReport()
+                  this.props.history.push('/signup')
+                }} >
+                <Icon name='home'/>Signup
+              </Menu.Item>,
+              <Menu.Item
+                name='login'
+                active={activeItem === 'login'}
+                onClick={(e, { name }) => {
+                  this.setState({ activeItem: name })
+                  childProps.rmReport()
+                  this.props.history.push('/login')
+                }} >
+                <Icon name='block layout'/>Login
+              </Menu.Item>
+            ]
+
+          }
+          <Menu.Item
+            onClick={(e, { name })=> {
+              this.props.history.push('/home')
+              this.setState({ sidebarVisible: false })
+            }}>
+            <Icon name='heartbeat'/>
+              <span className='text'>Accuracy</span>
+          </Menu.Item>
+          <Menu.Menu position='right'>
+            {childProps.isAuthenticated
+              ?
+              <Menu.Item
+                onClick={(e, { name })=> {
+                  this.props.history.push('/tcsbyplant')
+                  this.setState({ sidebarVisible: false })
+                }}>
+                <Icon name='block layout'/>
+                  <span className='text'>Logout&nbsp;&nbsp;&nbsp;</span>
+              </Menu.Item>
+              : ''
+            }
+          </Menu.Menu>
+        </Menu>
+
+        {this.state.sidebar === 'production' ?
+          <Sidebar.Pushable as={Segment} attached='bottom'>
+            <Sidebar as={Menu} animation='push' width='thin' visible={this.state.sidebarVisible} icon='labeled' vertical inverted>
+              <Menu.Item onClick={() => {
+                this.props.history.push('/tcsbyplant')
+                this.setState({ sidebarVisible: false })
+                this.setRptStep(1)
+              }}>
+                <Icon name='block layout'/>ToolCost
+              </Menu.Item>
+              <Menu.Item onClick={() => {
+                this.props.history.push('/tcsbyplant')
+                this.setState({ sidebarVisible: false })
+                this.setRptStep(1)
+              }}>
+                <Icon name='home'/>Excel
+              </Menu.Item>
+            </Sidebar>
+            <Sidebar.Pusher dimmed={this.state.sidebarVisible} style={divStyle} >
+              <Segment style={divStyle} basic className='container fill mycontainer'>
+        {this.state.rptStep === '1' ?
+                <Routes childProps={childProps} />
+                : ''
+              }
+                <div id='detail' style={divStyle} className='container fill mycontainer' />
+              </Segment>
+            </Sidebar.Pusher>
+          </Sidebar.Pushable>
+          :
+          <Sidebar.Pushable as={Segment} attached='bottom'>
+            <Sidebar as={Menu} animation='push' width='thin' visible={this.state.sidebarVisible} icon='labeled' vertical inverted>
+              <Menu.Item onClick={() => {
+                this.props.history.push('/tcsbyplant')
+                this.setState({ sidebarVisible: false })
+                this.setRptStep(1)
+              }}>
+                <Icon name='home'/>ToolCost
+              </Menu.Item>
+              <Menu.Item onClick={() => {
+                this.props.history.push('/tcsbyplant')
+                this.setState({ sidebarVisible: false })
+                this.setRptStep(1)
+              }}>
+                <Icon name='block layout'/>Excel
+              </Menu.Item>
+            </Sidebar>
+            <Sidebar.Pusher dimmed={this.state.sidebarVisible} style={divStyle} >
+              <Segment style={divStyle} basic className='container fill mycontainer'>
+                <Routes childProps={childProps} />
+                <div id='detail' style={divStyle} className='container fill mycontainer' />
+              </Segment>
+            </Sidebar.Pusher>
+          </Sidebar.Pushable>
+        }
+      </div>
+    )
+  }
+}
+
+export default withRouter(App)
+
+/*
+      <Menu fluid borderless inverted  secondary attached='bottom' widths={3}>
+        <Menu.Item name='buy' color='teal' active={activeItem === 'buy'} onClick={this.handleItemClick} />
+        <Menu.Item name='sell' color='teal' active={activeItem === 'sell'} onClick={this.handleItemClick} />
+        <Menu.Item name='rent' color='teal' active={activeItem === 'rent'} onClick={this.handleItemClick} />
+      </Menu>
+*/
+
+/*
+
+        {this.state.sidebar === 'production' ?
+          <Sidebar.Pushable as={Segment} attached='bottom'>
+            <Sidebar as={Menu} animation='push' width='thin' visible={this.state.sidebarVisible} icon='labeled' vertical inverted>
+              <Menu.Item onClick={() => {
+                this.props.history.push('/tcsbyplant')
+                this.setState({ sidebarVisible: false })
+                this.setRptStep(1)
+              }}>
+                <Icon name='block layout'/>ToolCost
+              </Menu.Item>
+              <Menu.Item onClick={() => {
+                this.props.history.push('/tcsbyplant')
+                this.setState({ sidebarVisible: false })
+                this.setRptStep(1)
+              }}>
+                <Icon name='home'/>Excel
+              </Menu.Item>
+            </Sidebar>
+            <Sidebar.Pusher dimmed={this.state.sidebarVisible} style={divStyle} >
+              <Segment style={divStyle} basic className='container fill mycontainer'>
+                <Routes childProps={childProps} />
+                <div id='detail' style={divStyle} className='container fill mycontainer' />
+              </Segment>
+            </Sidebar.Pusher>
+          </Sidebar.Pushable>
+          :
+          <Sidebar.Pushable as={Segment} attached='bottom'>
+            <Sidebar as={Menu} animation='push' width='thin' visible={this.state.sidebarVisible} icon='labeled' vertical inverted>
+              <Menu.Item onClick={() => {
+                this.props.history.push('/tcsbyplant')
+                this.setState({ sidebarVisible: false })
+                this.setRptStep(1)
+              }}>
+                <Icon name='home'/>ToolCost
+              </Menu.Item>
+              <Menu.Item onClick={() => {
+                this.props.history.push('/tcsbyplant')
+                this.setState({ sidebarVisible: false })
+                this.setRptStep(1)
+              }}>
+                <Icon name='block layout'/>Excel
+              </Menu.Item>
+            </Sidebar>
+            <Sidebar.Pusher dimmed={this.state.sidebarVisible} style={divStyle} >
+              <Segment style={divStyle} basic className='container fill mycontainer'>
+                <Routes childProps={childProps} />
+                <div id='detail' style={divStyle} className='container fill mycontainer' />
+              </Segment>
+            </Sidebar.Pusher>
+          </Sidebar.Pushable>
+        }
+
+
+      <SplitPane split='horizontal' allowResize={false} defaultSize={40}>
+        <div key='1'>
+          <MyNavBar childProps={childProps} />
+        </div>
+        <SplitPane split='vertical' allowResize={true} defaultSize={200}>
+          <div key='2' >
+            <MyAccord childProps={childProps} />
+          </div>
+	        <SplitPane split='vertical' allowResize={true} defaultSize={200}>
+	          <div key='4' >
+	            <Routes childProps={childProps} />
+	          </div>
+	          <div key='3' className='container fill' >
+	            <div  id='detail' />
+	          </div>
+	        </SplitPane>
+	        </SplitPane>
+	      </SplitPane>
+
+      <SplitPane split='horizontal' allowResize={false} defaultSize={40}>
+        <div key='1'>
+          <MyNavBar childProps={childProps} />
+        </div>
+        <SplitPane onChange={ this.handleResize } split='vertical' allowResize={true} defaultSize={200}>
+          <div key='2' >
+            <MyAccord childProps={childProps} />
+          </div>
+          <div key='3' >
+            <Routes childProps={childProps} />
+            <div id='detail' />
+          </div>
+        </SplitPane>
+      </SplitPane>
+
+    <SplitPane split="horizontal" allowResize={false} defaultSize={70}>
+        <div key="1">
+          <MyNavBar childProps={childProps} />
+        </div>
+        <SplitPane split="vertical" defaultSize={300}>
+            <div key="2" >
+              <MyAccord childProps={childProps} />
+            </div>
+            <div key="3" >
+              <Routes childProps={childProps} />
+               <div id='detail'></div>
+            </div>
+        </SplitPane>
+    </SplitPane>
+
+*/
+
---
+++ reporter/data/scripts/Json/content.js
@@ -0,0 +1,104 @@
+var sql = require('mssql');
+var Moment = require('moment');
+
+
+var config = {
+    user: 'sa',
+    password: 'buschecnc1',
+    //  server: '192.168.254.36', // You can use 'localhost\\instance' to connect to named instance
+    //  server: '10.1.2.19', // You can use 'localhost\\instance' to connect to named instance
+    server: 'busche-sql-1',
+    database: 'M2MDATA01',
+    //  database: 'm2mdata02',
+    port: 1433,
+    connectionTimeout:25000,
+    requestTimeout:30000,
+    //    debug: true,
+    options: {
+        encrypt: false // Use this if you're on Windows Azure
+        // ,instanceName: 'SQLEXPRESS'
+    }
+}
+var cribDefTO = {
+    user: 'sa',
+    password: 'buschecnc1',
+    server: '10.1.2.17',
+    options: {
+        database: 'Cribmaster',
+        port: 1433
+    }
+}
+
+function beforeRender(done) {
+    
+     //       var dtStart =Moment(new Date()).format("MM-DD-YYYY hh:mm:ss");
+    //       var dtEnd =Moment(new Date()).format("MM-DD-YYYY hh:mm:ss");
+    console.log('hello2');
+    //        done();
+
+    sql.connect(config).then(function(err) {
+        console.log(err);
+    //    var rptName = request.data.rptName;
+        var reqSql = new sql.Request();
+var notUpToDate =0;
+
+reqSql.query('select count(*) as notCurrent from ssis where uptodate = 0', 
+    (err, result) => {
+    // ... error checks
+
+    console.log(result.recordset[0].notCurrent) // return 1
+    
+            console.log(result.recordsets.length) // count of recordsets returned by the procedure
+            console.log(result.recordsets[0].length) // count of rows contained in first recordset
+            console.log(result.recordset) // first recordset from result.recordsets
+            console.log(result.returnValue) // procedure return value
+            console.log(result.output) // key/value collection of output values
+            console.log(result.rowsAffected) // array of numbers, each number represents the number of rows affected by executed statemens
+
+        var reqSql = new sql.Request();            
+            reqSql.query('select * from btreportacc', (err, result) => {
+                // ... error checks
+            
+                console.log(result.recordset[0].NoToolListCnt) // return 1
+                var myJSON = JSON.stringify(result.recordset[0]);
+                console.log(myJSON)
+                        console.log(result.recordsets.length) // count of recordsets returned by the procedure
+                        console.log(result.recordsets[0].length) // count of rows contained in first recordset
+                        console.log(result.recordset) // first recordset from result.recordsets
+                        console.log(result.returnValue) // procedure return value
+                        console.log(result.output) // key/value collection of output values
+                        console.log(result.rowsAffected) // array of numbers, each number represents the number of rows affected by executed statemens
+                        request.data = {
+                            summary: result.recordset,
+                            myJSON: myJSON
+                        };
+                    
+                        done();
+            })
+
+
+})
+
+/*
+reqSql.query('select * from btreportacc', (err, result) => {
+    // ... error checks
+
+    console.log(result.recordset[0].NoToolListCnt) // return 1
+    
+            console.log(result.recordsets.length) // count of recordsets returned by the procedure
+            console.log(result.recordsets[0].length) // count of rows contained in first recordset
+            console.log(result.recordset) // first recordset from result.recordsets
+            console.log(result.returnValue) // procedure return value
+            console.log(result.output) // key/value collection of output values
+            console.log(result.rowsAffected) // array of numbers, each number represents the number of rows affected by executed statemens
+            request.data = {
+                summary: result.recordset,
+                data: request.data
+            };
+        
+            done();
+})
+*/
+    }).catch(done);
+
+}
